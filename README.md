348a67dac94954977b33961449d6252e91a8c8e5
Maratib Ali Khan <maratib@gmail.com>
2025-04-23 02:19:18 +0500

    Docs updated

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # PHP Starter
-With `phpunit`
+With `phpunit` and `pint` pre-configured
+
+### Run tests
+```bash 
+composer run-script test
+```
+
+
+## Packagist package creation.
+
+1. Create your github repo
+2. Login to packagist.org
+3. Submit and provide the repo link
+
+
+### Versioning
+```bash
+git tag v0.0.1
+git push --tags -u origin main
+```
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -8,13 +8,25 @@ use PHPUnit\Framework\TestCase;
 class UserTest extends TestCase {
 
 
-    public function testViewInstantiation() {
+    public function testInit() {
         // Create an instance of the View class
         $user = new User('John');
         
+        
+        
+        
+        ob_start();
+        echo "HI!";
+        print_r("HI!");
+        var_dump("HI!");
+
+        ob_end_clean();
+
+        // ob_flush();
+
 
         // Check if the instance is of the correct class
-        // $this->assertInstanceOf('View', $view);
+        $this->assertInstanceOf(User::class, $user);
 
         $this->assertSame('John', $user->getName());
     }
